588521ec9246443e7982a770b3a285dce61db295
vivekdj417-sys <vivekdj417@gmail.com>
2026-04-08 19:58:31 +0530

    All are added

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+ 
+const connectDB=async()=>{
+    try {
+      const ya=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      console.log(`Mongodb connected DB host:${ya.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error",error);
+        process.exit(1);
+    }
+}
+export default connectDB;
